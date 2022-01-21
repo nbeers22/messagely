6d60724eafaa5d23f71b200d3fec33c0d50de72c
Nathan Beers <nathan.beers@oyova.com>
2022-01-20 19:18:14 -0500

    Reconfigured tailwind.

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Login from './Login';
+import Navbar from './Navbar';
 
 function App() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
+        <div className="app-container">
+            {/* <Navbar /> */}
+            <Login />
         </div>
     );
 }
